03dfd896287c8e745679427364989f09ff32f2a5
marcoscassone02 <ma.cassone@alumno.um.edu.ar>
2025-11-05 15:38:38 -0300

    tarea terminada

--- composeApp/src/commonMain/kotlin/org/basic/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/basic/project/App.kt
@@ -3,6 +3,7 @@ package org.basic.project
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -17,10 +18,16 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -29,7 +36,15 @@ import kmpbasico.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 fun App() {
-    MaterialTheme {
+    Navigator(
+        screen = Mainscreen()
+    )
+}
+
+class Mainscreen : Screen {
+    @Composable
+    override fun Content() {
+        val navigator: Navigator = LocalNavigator.currentOrThrow
         var name: String by remember { mutableStateOf("") }
         var marcos: String by remember { mutableStateOf("") }
         Column(modifier = Modifier
@@ -75,9 +90,62 @@ fun App() {
 
                 )
             }
+            Spacer(modifier = Modifier.height(180.dp))
 
+            Button(
+                onClick = {
+                    navigator.push(Secondscreen())
+                    }
+                ) {
+                Text(
+                    text = "Navegando"
+                )
 
+            }
+        }
+    }
+
+}
+
+class Secondscreen : Screen {
+    @Composable
+    override fun Content() {
+
+        val navigator : Navigator = LocalNavigator.currentOrThrow
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Red),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+            Text(
+                "Segunda Pantalla",
+                fontSize = 20.sp,
+                color = Color.Black
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .height(20.dp)
+            )
+
+            Button(
+                onClick = {
+                    navigator.pop()
+                }
+            ) {
+                Text(
+                    text = "Vamos para atrás"
+                )
+            }
+        }
 
     }
 }
-}
+
+
+
+
+
